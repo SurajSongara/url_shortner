87dd0b6135530302b497a38586ad73e62662b0c8
SurajSongara <surajskushwahcoder00@gmail.com>
2021-12-09 21:06:35 +0530

    basic commit

--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'url_shortner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
